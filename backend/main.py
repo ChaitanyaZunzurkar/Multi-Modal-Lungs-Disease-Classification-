3be9ccb6085ae25861bd87aa08ded32ee5f68ff6
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2026-04-06 14:09:19 +0530

    Fix api url

--- backend/main.py
+++ backend/main.py
@@ -204,7 +204,7 @@ origins = [origin.strip() for origin in raw_origins.split(",")]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
